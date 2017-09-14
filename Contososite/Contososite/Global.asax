3b9269620b06674721df2480d173abf916f85551
MD. JULHAS HOSSAIN (MOHON) <eyaminkhan00@gmail.com>
2017-09-15 05:56:09 +0600

    Learned how to control database first Entity Framework project

---
+++ Contososite/Contososite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Contososite.MvcApplication" Language="C#" %>
---
+++ Contososite/Contososite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Contososite.Startup))]
+namespace Contososite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
